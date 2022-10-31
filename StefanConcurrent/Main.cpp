7c84434f7057f32850e8f767c4862101d9a91efc
alicia22nzd <alicia22nzd@gmail.com>
2022-10-31 14:35:47 +0300

    update main

--- StefanConcurrent/Main.cpp
+++ StefanConcurrent/Main.cpp
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <iostream>
+#include "rectangle.h"
 
 using namespace std;
+
+void TestRectangle()
+{
+
+	Rectangle mn(17, 5);
+	cout << "Areas is: " << mn.GetArea() << endl;
+	cout << "Perimeter is: " << mn.GetPerimeter() << endl;
+}
 
 void main()
 {
 	//polymorphism = multiple functions with the same name but different arguments
 	//testMatrix();
 	//testPointer();
-
+	TestRectangle();
 	cout << "Program Ended" << endl;
 	//return EXIT_SUCCESS;
 	system("pause");//prevent console window to close
