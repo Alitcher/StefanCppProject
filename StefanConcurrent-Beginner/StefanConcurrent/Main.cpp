063defa7a0f7cb0f401ec28a4e0c0cfd34b3c7ba
alicia22nzd <alicia22nzd@gmail.com>
2022-10-31 14:03:22 +0300

    relocate project

---
+++ StefanConcurrent-Beginner/StefanConcurrent/Main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	//polymorphism = multiple functions with the same name but different arguments
+	//testMatrix();
+	//testPointer();
+
+	cout << "Program Ended" << endl;
+	//return EXIT_SUCCESS;
+	system("pause");//prevent console window to close
+}
